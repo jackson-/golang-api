9996bcaf802152fe8141e03fc6c79b7a5edde8b7
jackson- <jackson.t.devin@gmail.com>
2017-11-27 21:49:19 -0500

    init commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// a := App{}
+	// a.Initialize(
+	// 	os.Getenv("APP_DB_USERNAME"),
+	// 	os.Getenv("APP_DB_PASSWORD"),
+	// 	os.Getenv("APP_DB_NAME"))
+	fmt.Println("WE HERE")
+	// a.Run(":8080")
+}
